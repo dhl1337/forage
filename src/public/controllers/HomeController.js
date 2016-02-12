0fe1061860174d8a9632267ccb4cbc051e2149d2
Dan Le <dhl1337@gmail.com>
2016-02-12 13:58:24 -0700

    trying to hide free user markers

--- src/public/controllers/HomeController.js
+++ src/public/controllers/HomeController.js
@@ -181,13 +181,16 @@
         // Google Maps UI
 
         function userLocationUpdate(userInfo){
+            if (userInfo.name == 'Anonymous') {
+                console.log('dont show me')
+            }
             if(!users[userInfo.id]) users[userInfo.id] = { id: userInfo.id };
 
             users[userInfo.id].name = userInfo.name;
             users[userInfo.id].latitude  = userInfo.latitude;
             users[userInfo.id].longitude = userInfo.longitude;
             users[userInfo.id].timestamp = new Date().getTime();
-            //console.log(userInfo);
+            //console.log("this is userinfo",userInfo);
             refreshMarkers();
         }
         function refreshMarkers(){
@@ -198,16 +201,43 @@
                 if(userInfo.marker){
                     // If we havn't received any update from the user
                     //  We remove the marker of missing user
+                    console.log("this is current user info",currentUserInfo.id);
+                    console.log("this is userInfo.id", userInfo.id);
                     if( userInfo.id != currentUserInfo.id &&
                         userInfo.timestamp + 1000*30 < new Date().getTime() ){
                         userInfo.marker.setMap(null);
                         delete users[id];
                         continue;
                     }
-                }else{
+                }else if (vm.openTruck)  {
                     // Create a marker for the new user
                     var marker = new google.maps.Marker({ map:map });
+
                     google.maps.event.addListener(marker, 'click', function() {
+                        //var contentString = '<div id="content">'+
+                        //    '<div id="siteNotice">'+
+                        //    '</div>'+
+                        //    '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+                        //    '<div id="bodyContent">'+
+                        //    '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+                        //    'sandstone rock formation in the southern part of the '+
+                        //    'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+                        //    'south west of the nearest large town, Alice Springs; 450&#160;km '+
+                        //    '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+                        //    'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+                        //    'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+                        //    'Aboriginal people of the area. It has many springs, waterholes, '+
+                        //    'rock caves and ancient paintings. Uluru is listed as a World '+
+                        //    'Heritage Site.</p>'+
+                        //    '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+                        //    'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+                        //    '(last visited June 22, 2009).</p>'+
+                        //    '</div>'+
+                        //    '</div>';
+                        //var infowindow = new google.maps.InfoWindow({
+                        //    content: contentString
+                        //});
+                        //console.log("this is the marker",marker);
                         infowindow.setContent(marker.getTitle());
                         infowindow.open(map, marker);
                     });
