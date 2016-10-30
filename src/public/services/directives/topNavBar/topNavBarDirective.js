ccc41a2486beb45455927100ba2ca1440be88790
Dan Le <dhl1337@gmail.com>
2016-10-30 18:00:19 -0500

    change nav bar to es6

--- src/public/services/directives/topNavBar/topNavBarDirective.js
+++ src/public/services/directives/topNavBar/topNavBarDirective.js
@@ -1,40 +1,30 @@
-(function () {
+(() => {
     'use strict';
     angular
         .module('forageApp')
         .directive('topNavBar', topNavBar);
 
-    function topNavBar () {
-        var directive = {
+    function topNavBar() {
+        const directive = {
             templateUrl: './services/directives/topNavBar/topNavBar.html',
             controller: topNavBarController,
             controllerAs: 'topNavBar'
         };
 
-        function topNavBarController (HomeService, FoodtruckService) {
-            var vm = this;
-            vm.loginModal = loginModal;
-            vm.dropDown = dropDown;
+        function topNavBarController(HomeService, FoodtruckService) {
 
+            const vm = this;
 
-            HomeService.getCurrentuser().then(function(user){
+            HomeService.getCurrentuser().then(user => {
                 vm.user = user;
-                if (vm.user && vm.user.foodTruck) {
-                    FoodtruckService.getFoodtruckId(vm.user.foodTruck).then(function(foodtruckUser){
-                        vm.foodtruckUser = foodtruckUser;
-                    });
-                }
+                if (vm.user && vm.user.foodTruck)
+                    FoodtruckService.getFoodtruckId(vm.user.foodTruck).then(foodtruckUser => vm.foodtruckUser = foodtruckUser);
             });
 
-            function dropDown () {
-                $('.ui.top.right.pointing.dropdown')
-                    .dropdown({
-                        allowAdditions: false
-                    })
-                ;
-            }
+            vm.dropDown = () => $('.ui.top.right.pointing.dropdown').dropdown({allowAdditions: false});
 
         }
+
         return directive;
     }
 })();
