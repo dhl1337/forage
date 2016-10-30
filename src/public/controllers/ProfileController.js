d7a6cba83aa39f0fc447d667810029b034a3bc16
Dan Le <dhl1337@gmail.com>
2016-10-30 17:29:04 -0500

    change profile controller to es6

--- src/public/controllers/ProfileController.js
+++ src/public/controllers/ProfileController.js
@@ -1,36 +1,33 @@
-(function () {
+(() => {
     angular
         .module('forageApp')
         .controller('ProfileController', ['HomeService', 'FoodtruckService', 'ProfileService', 'currentUser', ProfileController]);
 
-    function ProfileController (HomeService, FoodtruckService, ProfileService, currentUser) {
-        console.log('hello');
-        var vm = this;
+    function ProfileController(HomeService, FoodtruckService, ProfileService, currentUser) {
+        const vm = this;
+
         vm.currentUserId = currentUser._id;
 
         google.maps.visualRefresh = true;
-        HomeService.getCurrentUserId(vm.currentUserId).then(function(user){
-            vm.user = user;
-        });
 
-        FoodtruckService.getFoodtruck().then(function (foodtruck) {
-            vm.foodtruck = foodtruck;
-        });
+        HomeService.getCurrentUserId(vm.currentUserId).then(user => vm.user = user);
 
-        vm.foodtrucksFavorite = function () {
+        FoodtruckService.getFoodtruck().then(foodtruck => vm.foodtruck = foodtruck);
+
+        vm.foodtrucksFavorite = () => {
             vm.arr = [];
-            for (var i = 0; i < vm.user.length; i++) {
-                for (var key in vm.user[i].favorites) {
+            for (let i = 0; i < vm.user.length; i++) {
+                for (let key in vm.user[i].favorites) {
                     vm.arr.push(vm.user[i].favorites[key]['foodtruckId']);
                 }
             }
         };
 
-        ProfileService.getReview(vm.currentUserId).then(function (foodtruck) {
+        ProfileService.getReview(vm.currentUserId).then(foodtruck => {
             vm.foodtruck = foodtruck;
             vm.foundReview = [];
-            for (var i = 0; i < vm.foodtruck.length; i++) {
-                for (var j = 0; j < vm.foodtruck[i].reviews.length; j++) {
+            for (let i = 0; i < vm.foodtruck.length; i++) {
+                for (let j = 0; j < vm.foodtruck[i].reviews.length; j++) {
                     if (vm.foodtruck[i].reviews[j].userId == vm.user[0]._id) {
                         vm.foundReview.push(vm.foodtruck[i].reviews[j]);
                     }
@@ -39,7 +36,7 @@
         });
 
 
-        $('#userMenu').on('click','div', function(){
+        $('#userMenu').on('click', 'div', () => {
             $(this).addClass('active').siblings().removeClass('active');
         });
     }
