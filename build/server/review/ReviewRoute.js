828fcb86e2cb245329a318f51966df32b71f8b08
Dan Le <dhl1337@gmail.com>
2016-11-03 13:03:28 -0500

    initial commit

--- build/server/review/ReviewRoute.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var _ReviewController = require('./ReviewController');
-
-module.exports = function (app) {
-    app.get('/api/reviews/:id', _ReviewController.getReview);
-    app.post('/api/reviews/:id', _ReviewController.addReview);
-};
