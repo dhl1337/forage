c61e54ba129d7167525170ec924d25370e8b3825
Dan Le <dhl1337@gmail.com>
2016-11-03 12:56:04 -0500

    initial commit

--- src/server/server.js
+++ src/server/server.js
@@ -18,7 +18,7 @@ mongoose.connection.once('open', () => console.log("Successfully connected to mo
 require('./configs/passport.js')(passport);
 
 // Express Middleware
-app.use(expressSession(process.env.SESSION_SECRET));
+app.use(expressSession(process.env.SESSION_SECRET || config.session.secret));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(bodyParser.json());
