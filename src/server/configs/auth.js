d69758bfc80ab36a86a78c02497041a581be352f
Dan Le <dhl1337@gmail.com>
2016-11-03 13:07:19 -0500

    initial commit

---
+++ src/server/configs/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'facebookAuth' : {
+        clientID: '759618597501870',
+        clientSecret: '7dbef35385e8feffc4266a5112c9bd73',
+        callbackURL: "http://localhost:9999/auth/facebook/callback"
+    }
+};
