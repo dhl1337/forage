38d98ffe0016dcdeb21dcf6da13276e218592a81
Dan Le <dhl1337@gmail.com>
2016-11-05 17:41:39 -0500

    remove facebook config

--- src/server/configs/auth.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    'facebookAuth' : {
-        clientID: '759618597501870',
-        clientSecret: '7dbef35385e8feffc4266a5112c9bd73',
-        callbackURL: "http://localhost:9999/auth/facebook/callback"
-    }
-};
